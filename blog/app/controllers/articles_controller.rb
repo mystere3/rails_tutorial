e85cc0b3d73f290de7e5694bbd151ce6b5858104
mystere3 <mystere3@msn.com>
2015-04-09 05:29:59 +0000

    Finished step 5. Ready to start 6.

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -9,6 +9,11 @@ class ArticlesController < ApplicationController
   end
   
   def new
+    @article = Article.new
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
   end
   
   def create
@@ -19,6 +24,23 @@ class ArticlesController < ApplicationController
     else
       render 'new'
     end
+  end
+  
+  def update
+    @article = Article.find(params[:id])
+    
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    
+    redirect_to articles_path
   end
   
   private
