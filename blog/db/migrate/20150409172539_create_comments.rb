aa6984706f04c6eb9a6265f4e5fecde9f78585a3
mystere3 <mystere3@msn.com>
2015-04-09 16:56:14 -0400

    working. Finished step 6. Start 7 next.

--- blog/db/migrate/20150409172539_create_comments.rb
+++ blog/db/migrate/20150409172539_create_comments.rb
@@ -7,5 +7,6 @@ class CreateComments < ActiveRecord::Migration
 
       t.timestamps
     end
+    # add_foreign_key :comments, :articles  # MIGRATE HAS A HARD TIME WITH THIS. THINK ITS FOR RAILS 4.2.0 and up only.
   end
 end
