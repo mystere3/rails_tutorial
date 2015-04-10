37d18971d71296d5fa89ccf041fde1d527cf1e81
mystere3 <mystere3@msn.com>
2015-04-10 20:46:54 +0000

    figured out and added some styles and images. End of work day.

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -13,14 +13,14 @@
 
 ActiveRecord::Schema.define(version: 20150409172539) do
 
-  create_table "articles", force: true do |t|
-    t.string   "title"
+  create_table "articles", force: :cascade do |t|
+    t.string   "title",      limit: 255
     t.text     "text"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "comments", force: true do |t|
+  create_table "comments", force: :cascade do |t|
     t.string   "commenter"
     t.text     "body"
     t.integer  "article_id"
